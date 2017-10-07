6e241007ee2c0c7a6db7877910640367bf99aeaa
qiang-yu <secrect@gmail.com>
2017-10-07 15:27:07 +0800

    add auth

--- src/auth.h
+++ src/auth.h
@@ -6,7 +6,10 @@
 #define SECTUN_GITHUB_AUTH_H
 
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include <netdb.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
