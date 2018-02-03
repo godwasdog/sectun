e28a08e335e6e7865ab36ab2395481d949db5095
qiang-yu <secrect@gmail.com>
2018-02-03 17:31:52 +0800

    update test file to make gfwlist test pass

--- test/start_client-gfwlist.sh
+++ test/start_client-gfwlist.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
 
+############################# these code only for testing purpose #####################
+## set default route
+ip route add   0/1 dev eth0
+ip route add   128/1 dev eth0
+
+## add baidu ip to list
+ipset -A  gfwlist  14.215.177.38
+
+############################# these code only for testing purpose #####################
+
+
+
+
+
+
 ./sectun -c client-gfwlist-config.json -s start -v
 
+
+
+
+
+
+############################# these code only for testing purpose #####################
+
+## remove default route
+ip route del   0/1 dev eth0
+ip route del   128/1 dev eth0
+
+############################# these code only for testing purpose #####################
